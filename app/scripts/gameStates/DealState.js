a452f1ff1984a2a495235472dbe28f983e5e67e5
Lucas Paulger <lucas.paulger@gmail.com>
2014-10-02 13:58:53 +0300

    unit test DealState

--- app/scripts/gameStates/DealState.js
+++ app/scripts/gameStates/DealState.js
@@ -2,6 +2,7 @@ define(['gameStates/BaseState', 'modules/DeckModule'],function(BaseState, Deck){
   'use strict';
   function DealState(game){
     BaseState.call(this, game, 'Deal');
+    this.game.$deck = new Deck();
   }
 
   DealState.prototype = Object.create(BaseState.prototype);
@@ -9,9 +10,7 @@ define(['gameStates/BaseState', 'modules/DeckModule'],function(BaseState, Deck){
 
   DealState.prototype.init = function(){
     this.game.$player2HandVisible = this.game.options.showOpponentsHand;
-    this.game.$deck = new Deck();
     this.game.$deck.shuffle();
-
     var hands = this.game.$deck.deal();
     this.p1.hand = hands.bottomHand.sort(sortByValue);
     this.p2.hand = hands.topHand.sort(sortByValue);
--- test/spec/gameStatesSpec/DealStateSpec.js
+++ test/spec/gameStatesSpec/DealStateSpec.js
@@ -1,7 +1,62 @@
-define(['gameStates/DealState'], function() {
+define(['gameStates/DealState'], function(DealState) {
   'use strict';
 
   describe('DealState', function () {
+    var game = {
+      $player1: {
+        possessive: 'your'
+      },
+      $deck: function(){},
+      $player2: {},
+      options: {
+        showOpponentsHand: false
+      },
+      transitionTo: function(){}
+    },
+    dealState;
 
+    describe('Constructor', function(){
+      it('should create DealState Obj', function(){
+        dealState = new DealState(game);
+        expect(dealState).toBeDefined();
+        expect(typeof dealState).toEqual('object');
+      });
+    });
+    
+    describe('init', function(){
+      beforeEach(function(){
+        spyOn(game, 'transitionTo');
+        dealState = new DealState(game);
+        spyOn(game.$deck, 'shuffle');
+        spyOn(game.$deck, 'deal').and.returnValue({
+          topHand: [{faceValue: 1},{faceValue: 4},{faceValue: 1},{faceValue: 2},{faceValue: 6},{faceValue: 1}],
+          bottomHand: [{faceValue: 10},{faceValue: 1},{faceValue: 6},{faceValue: 6},{faceValue: 9},{faceValue: 1}]
+        });
+        game.$cribOwner = game.$player1;
+        dealState.init();
+      });
+      
+      it('should create a deck', function(){
+        expect(game.$deck).toBeDefined();
+      });
+      
+      it('should shuffle the deck', function(){
+        expect(game.$deck.shuffle).toHaveBeenCalled();
+        expect(game.$deck.shuffle.calls.count()).toEqual(1);
+      });
+      
+      it('should deal two hands', function(){
+        expect(game.$deck.deal).toHaveBeenCalled();
+        expect(game.$deck.deal.calls.count()).toEqual(1);
+      });
+
+      it('should display a message', function(){
+        expect(game.$messages[0]).toEqual('select two cards for your crib');
+      });
+      
+      it('should transitionTo Crib State', function(){
+        expect(game.transitionTo).toHaveBeenCalledWith('Crib');
+      });
+    });
   });
 });
