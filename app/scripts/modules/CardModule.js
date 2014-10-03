16ec06b0d9ea52e8f733284369640de6cfc2d778
Lucas Paulger <lucas.paulger@gmail.com>
2014-10-03 17:10:56 +0300

    spacing

--- app/scripts/modules/CardModule.js
+++ app/scripts/modules/CardModule.js
@@ -1,32 +1,32 @@
 define([], function () {
-    'use strict';
+  'use strict';
 
-    function Card(value, type){
-      this.faceValue = value;
-      this.value = setValue(value);
-      this.suit = type;
-      this.name = setName(value);
+  function Card(value, type){
+    this.faceValue = value;
+    this.value = setValue(value);
+    this.suit = type;
+    this.name = setName(value);
+  }
+
+  function setName(value){
+    switch(value){
+      case 1:
+        return 'A';
+      case 11:
+        return 'J';
+      case 12:
+        return 'Q';
+      case 13:
+        return 'K';
+      default:
+        return value.toString();
     }
+  }
 
-    function setName(value){
-      switch(value){
-        case 1:
-          return 'A';
-        case 11:
-          return 'J';
-        case 12:
-          return 'Q';
-        case 13:
-          return 'K';
-        default:
-          return value.toString();
-      }
-    }
+  function setValue(value){
+    if(value > 10) return 10;
+    return value;
+  }
 
-    function setValue(value){
-      if(value > 10) return 10;
-      return value;
-    }
-
-    return Card;
-  });
+  return Card;
+});
