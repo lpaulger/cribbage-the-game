3633d3882c9b18621a3572af86c3bff9b609e775
Lucas Paulger <lucas.paulger@gmail.com>
2014-10-17 18:32:38 +0300

    fix lint warnings on codeclimate

--- app/scripts/modules/CardModule.js
+++ app/scripts/modules/CardModule.js
@@ -10,16 +10,16 @@ define([], function () {
 
   function setName(value){
     switch(value){
-      case 1:
-        return 'A';
-      case 11:
-        return 'J';
-      case 12:
-        return 'Q';
-      case 13:
-        return 'K';
-      default:
-        return value.toString();
+    case 1:
+      return 'A';
+    case 11:
+      return 'J';
+    case 12:
+      return 'Q';
+    case 13:
+      return 'K';
+    default:
+      return value.toString();
     }
   }
 
