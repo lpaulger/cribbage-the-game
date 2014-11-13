a902942dc638a549dee238a123ceb5cc0ba0b388
Lucas Paulger <lucas.paulger@gmail.com>
2014-11-13 17:03:27 +0200

    jshint unused var

--- test/spec/gameStatesSpec/SummaryStateSpec.js
+++ test/spec/gameStatesSpec/SummaryStateSpec.js
@@ -2,10 +2,6 @@ define(['scripts/gameStates/SummaryState'], function(SummaryState){
   'use strict';
 
   describe('SummaryState', function(){
-    var summaryState, game = {
-      $player1: {possessive: 'Your'},
-      $player2: {possessive: 'Robo\'s'}
-    };
     describe('Constructor', function(){
       it('should define the summary state', function(){
         summaryState = new SummaryState({});
