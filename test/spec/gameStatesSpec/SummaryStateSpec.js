e1f07444db07cf1f71b7e1a8e7ab27abbcb39c87
Lucas Paulger <lucas.paulger@gmail.com>
2014-11-13 17:05:04 +0200

    removed too much :)

--- test/spec/gameStatesSpec/SummaryStateSpec.js
+++ test/spec/gameStatesSpec/SummaryStateSpec.js
@@ -2,10 +2,10 @@ define(['scripts/gameStates/SummaryState'], function(SummaryState){
   'use strict';
 
   describe('SummaryState', function(){
+    var summaryState;
     describe('Constructor', function(){
       it('should define the summary state', function(){
         summaryState = new SummaryState({});
-
         expect(summaryState).toBeDefined();
       });
     });
