a93031dcfafe639bf46c57b6bbf971df47bd86f8
Lucas Paulger <lucas.paulger@gmail.com>
2014-11-13 17:26:57 +0200

    fix path

--- app/scripts/require.config.js
+++ app/scripts/require.config.js
@@ -1,7 +1,7 @@
 require.config({
   paths: {
     baseUrl: 'app',
-    jquery: 'modules/DOMulator',
+    jquery: 'scripts/modules/DOMulator',
     mustache: 'bower_components/mustache/mustache'
   },
   shim: {
