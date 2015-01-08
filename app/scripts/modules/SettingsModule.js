f6c5f2a27d7303829aeabe51a1f5bb79b43862fd
Lucas Paulger <lucas.paulger@gmail.com>
2015-01-08 12:32:25 +0200

    settings fix
    - shouldnt store setting name and description (UI only), only store id
    and value.
    - if I decide to update these strings in the future I wont have to worry
    about localstorage storing these ui values.

--- app/scripts/modules/SettingsModule.js
+++ app/scripts/modules/SettingsModule.js
@@ -1,6 +1,19 @@
 define(['modules/StorageModule'], function(Storage){
   'use strict';
   var _settings;
+
+  function restoreSettingInformation(self){
+    _settings.forEach(function(setting){
+      setting.name = self._defaultSettings.filter(function(obj){
+        return obj.id === setting.id;
+      })[0].name;
+
+      setting.description = self._defaultSettings.filter(function(obj){
+        return obj.id === setting.id;
+      })[0].description;
+    });
+  }
+
   return {
     _defaultSettings: [{
       id:'action-confirmation',
@@ -11,7 +24,7 @@ define(['modules/StorageModule'], function(Storage){
     {
       id:'manual-count',
       name:'Count Manually',
-      description: 'Player scores their own points',
+      description: 'Player scores their own points. Announcing \'Go\' will automatically score one (1) point',
       value: false
     }],
     get: function(settingId){
@@ -22,13 +35,23 @@ define(['modules/StorageModule'], function(Storage){
       })[0].value;
     },
     load: function(){
+      var self = this;
       _settings = Storage.loadSettings();
       if(!_settings)
         _settings = this._defaultSettings;
+
+      restoreSettingInformation(self);
+
       return _settings;
     },
     save: function(settings){
       _settings = settings;
+
+      _settings.forEach(function(setting){
+        delete setting.name;
+        delete setting.description;
+      });
+
       Storage.saveSettings(settings);
     }
   };
