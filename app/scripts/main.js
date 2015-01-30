11e429bf0cafeed4920c3ddbfbac2c183416a2e3
Lucas Paulger <lucas.paulger@gmail.com>
2015-01-30 16:09:29 +0200

    android 2.3 bugfix
    fixes #87 cribbage board, and cards with 1 pixel device ratio. Fixed
    deck.strait issue

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,16 @@
 require(['app'], function (App) {
   'use strict';
-  var app = new App();
-  app.init();
+  if(typeof cordova !== 'undefined'){
+    document.addEventListener('deviceready', function(){
+    
+      if(device.version.slice(0,2) === '2.')
+        document.getElementsByTagName('body')[0].className += 'old-android';
+
+      var app = new App();
+      app.init();
+    }, false);
+  } else {
+    var app = new App();
+    app.init();
+  }
 });
